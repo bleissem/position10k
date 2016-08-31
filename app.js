12985bc31147095977064b31c993b75965f4dda2
Alexander Bleissem <alexander@bleissem.com>
2016-08-31 22:11:02 +0200

    changed to express

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-var http = require('http');
+var express = require('express');
+
+var app = express();
 
 var port = process.env.PORT || 7000;
 
-var server = http.createServer(function(request, response){
-
+app.get('/', function (request,response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World\n");
+})
 
-});
 
-server.listen(port); 
+var server = app.listen(port, function () {
+
+})
